46336897a4f4977e0656d7ccb18f4231eeaaa779
thaqif <thaqifabdullah@gmail.com>
2016-10-11 23:11:44 +0200

    update search.c

--- search.c
+++ search.c
@@ -8,6 +8,8 @@
 
 struct search
 {
+	int portion;
+	int indice;
 	int x;
 	int *T;
 	int size;
@@ -36,12 +38,26 @@ void afficherT(int *T, int size){
 	}
 }
 
-int search(void *arg){
-	struct search *ma_recherce = (struct search *)arg;
-	
+void *search(void *arg){
+	struct search *s = (struct search *)arg;
+	pthread_t tid = pthread_self();
+	srand((int)tid); 
+	int i = s->portion;
+	while(i>0){
+		if(s->x == s->T[s->indice]){
+			printf("Processus %d Thread %x : Found !\n", getpid(), (unsigned int)tid);
+			usleep(rand()/RAND_MAX*1000000.);
+			return (void *)tid;
+		}
+		s->indice++;
+		i--;
+	}
+	printf("Processus %d Thread %x : Not found...\n", getpid(), (unsigned int)tid);
+	usleep(rand()/RAND_MAX*1000000.);
+	return (void *)tid;
 }
 
-int main(int argc, char const *argv[])
+int main(int argc, char **argv)
 {
 	/* code */
 	FILE *fichier;
@@ -50,10 +66,8 @@ int main(int argc, char const *argv[])
 	int nb_threads = 0 ;
   	pthread_t *tids ;
 
-  	struct search * s = malloc(sizeof(struct search));
-
 	if(argc != 3){
-		(stderr, "usage : %s fichier valeur_a_chercher\n", argv[0]); 
+		fprintf(stderr, "usage : %s fichier valeur_a_chercher\n", argv[0]); 
 		exit(-1);
 	}
 	fichier = fopen(argv[1], "r");
@@ -75,17 +89,36 @@ int main(int argc, char const *argv[])
 	T = init_vecteur(T,fichier);
 	afficherT(T,size);
 
+	struct search * s = malloc(sizeof(struct search));
+	if (s == NULL)
+	{
+		fprintf(stderr, "Erreur d'allocation de la structure search\n");
+		exit(3);
+	}
 	s->T = T;
-	s->size = size;
+	s->size = size;printf("Size : %d\n",size);
 	s->x = atoi(argv[2]); // l'élément cherché dans tab
+	s->portion = 2;
 
-	if(size >= 2){
-		nb_threads = size/2;
+	if(size >= s->portion){
+		nb_threads = size/s->portion;
 		tids = malloc (nb_threads*sizeof(pthread_t));
 		/* Create the threads */
-  		for (i = 0 ; i < nb_threads; i++){
+  		for (int i = 0 ; i < nb_threads; i++){
+  			s->indice = i*(s->portion);
+  			if(i==nb_threads-1)
+  				s->portion = s->size - s->indice;
+  			printf("indice = %d | s->portion = %d\n",s->indice,s->portion);
     		pthread_create (&tids[i], NULL, search, s);
   		}
+
+  		for (int i = 0; i < nb_threads; ++i)
+  		{
+  			pthread_join(tids[i], NULL);
+  		}
+
+  		free(tids);
+  		return EXIT_SUCCESS;
 	}	
 	return 0;
 }
